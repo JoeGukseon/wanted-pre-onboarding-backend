a6c805bb66c444a3bf6dce20a371a47d2d72dd9d
JoeGukSeon <zipcks1381@gmail.com>
2023-08-01 21:38:54 +0900

    :sparkles: feat : MemberController 생성 postMember 메서드 구현 (회원가입)

---
+++ src/main/java/com/board/member/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.board.member.controller;
+
+import com.board.member.dto.MemberDto;
+import com.board.member.entity.Member;
+import com.board.member.mapper.MemberMapper;
+import com.board.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@Validated
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final static String MEMBER_DEFAULT_URL = "/members";
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+
+    @PostMapping
+    public ResponseEntity<Void> postMember(@Valid @RequestBody MemberDto.Post requestBody) {
+
+        Member member = memberMapper.memberPostToMember(requestBody);
+
+        Member createdMember = memberService.createMember(member);
+        URI location = UriComponentsBuilder
+                .fromPath(MEMBER_DEFAULT_URL)
+                .path("/{id}")
+                .buildAndExpand(createdMember.getMemberId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
