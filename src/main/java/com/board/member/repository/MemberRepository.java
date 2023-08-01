4d3429f36ce152b6627942887ec1ec7958d2a232
JoeGukSeon <zipcks1381@gmail.com>
2023-08-01 21:40:22 +0900

    :sparkles: feat : MemberRepository 생성 findByEmail 생성

---
+++ src/main/java/com/board/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.board.member.repository;
+
+import com.board.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
