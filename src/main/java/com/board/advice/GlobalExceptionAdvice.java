6ed095a5a334bc5c930cec988a5a231639119471
JoeGukSeon <zipcks1381@gmail.com>
2023-08-01 21:43:27 +0900

    :sparkles: feat : GlobalExceptionAdvice 생성 예외처리

---
+++ src/main/java/com/board/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,44 @@
+package com.board.advice;
+
+import com.board.exception.BusinessLogicException;
+import com.board.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST,e.getBindingResult());
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST,e.getConstraintViolations());
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleBusinessLogicException(BusinessLogicException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode().getStatus()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+        e.printStackTrace();
+        return response;
+    }
+}
