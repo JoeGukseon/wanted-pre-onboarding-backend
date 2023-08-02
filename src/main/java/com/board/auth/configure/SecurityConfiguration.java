e99fbbf5b9a8a1610a8e26fdb2f17c87ff0c8885
JoeGukSeon <zipcks1381@gmail.com>
2023-08-02 16:03:32 +0900

    :sparkles: feat : SecurityConfiguration 추가

--- src/main/java/com/board/auth/configure/SecurityConfiguration.java
+++ src/main/java/com/board/auth/configure/SecurityConfiguration.java
@@ -1,10 +1,19 @@
 package com.board.auth.configure;
 
+import com.board.auth.filter.JwtAuthenticationFilter;
+import com.board.auth.filter.JwtAuthorizationFilter;
+import com.board.auth.handler.MemberAuthenticationEntryPoint;
+import com.board.auth.handler.MemberAuthenticationFailureHandler;
+import com.board.auth.handler.MemberAuthenticationSuccessHandler;
+import com.board.auth.jwt.JwtTokenizer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,6 +29,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final MemberAuthenticationFailureHandler memberAuthenticationFailureHandler;
+    private final MemberAuthenticationSuccessHandler memberAuthenticationSuccessHandler;
+    private final MemberAuthenticationEntryPoint memberAuthenticationEntryPoint;
+    private final JwtTokenizer jwtTokenizer;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final ObjectMapper objectMapper;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -31,8 +47,14 @@ public class SecurityConfiguration {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(memberAuthenticationEntryPoint)
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                                .anyRequest().permitAll()
+//                .anyRequest().authenticated()
                 );
 
         return http.build();
@@ -50,5 +72,21 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+
+    private class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer, objectMapper);
+
+            jwtAuthenticationFilter.setFilterProcessesUrl("/members/login");
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(memberAuthenticationFailureHandler);
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(memberAuthenticationSuccessHandler);
+
+            builder.addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtAuthorizationFilter, JwtAuthenticationFilter.class);
+        }
     }
 }
