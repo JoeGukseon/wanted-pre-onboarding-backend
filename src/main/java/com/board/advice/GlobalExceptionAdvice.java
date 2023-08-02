2419cd8842a4de32734284d022b858e36d3c3702
JoeGukSeon <zipcks1381@gmail.com>
2023-08-03 01:22:07 +0900

    :recycle: refactor : GlobalExceptionAdvice 수정 handleAuthenticationServiceException handleMissingRequestHeaderException 처리 추가

--- src/main/java/com/board/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/board/advice/GlobalExceptionAdvice.java
@@ -4,7 +4,9 @@ import com.board.exception.BusinessLogicException;
 import com.board.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +15,20 @@ import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleAuthenticationServiceException(AuthenticationServiceException e) {
+
+        return ErrorResponse.of(HttpStatus.UNAUTHORIZED,
+                e.getMessage());
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingRequestHeaderException(MissingRequestHeaderException e) {
+
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST,
+                e.getMessage());
+    }
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(
