b65484d66ba6c7f94f15426e2c55ab2c309a2cf6
JoeGukSeon <zipcks1381@gmail.com>
2023-08-03 01:50:00 +0900

    :recycle: refactor : PostRepository 수정 findById 추가(N+1 발생하지 않아 엔티티그래프 주석)

--- src/main/java/com/board/post/repository/PostRepository.java
+++ src/main/java/com/board/post/repository/PostRepository.java
@@ -3,8 +3,14 @@ package com.board.post.repository;
 import com.board.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
+
+//    @EntityGraph(attributePaths = "member")
+    Optional<Post> findById(Long postId);
 }
