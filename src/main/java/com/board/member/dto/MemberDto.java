f19908b9b363dfa2c6f0a83a250a3635a15eecdc
JoeGukSeon <zipcks1381@gmail.com>
2023-08-02 16:02:25 +0900

    :recycle: refactor : MemberDto 수정 Post - Builder -> Setter로 수정 Login DTO 추가

--- src/main/java/com/board/member/dto/MemberDto.java
+++ src/main/java/com/board/member/dto/MemberDto.java
@@ -1,18 +1,26 @@
 package com.board.member.dto;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Pattern;
 
 public class MemberDto {
     @Getter
-    @Builder
+    @Setter
     public static class Post {
         @Pattern(regexp = ".*@.*", message = "이메일에 @가 포함되어있지 않습니다")
         private String email;
 
         @Pattern(regexp = "^.{8,}$", message = "비밀번호는 8자 이상이여야 합니다.")
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    public static class Login {
+        private String email;
+
         private String password;
     }
 }
