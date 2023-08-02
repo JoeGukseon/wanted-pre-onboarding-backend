add2758a0c7e4a9dd26a013b0ac56f92d9883304
JoeGukSeon <zipcks1381@gmail.com>
2023-08-02 23:17:14 +0900

    :recycle: refactor : postMember 수정 UriComponentsBuilder -> UriCreator 유틸로 변경

--- src/main/java/com/board/member/controller/MemberController.java
+++ src/main/java/com/board/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.board.member.dto.MemberDto;
 import com.board.member.entity.Member;
 import com.board.member.mapper.MemberMapper;
 import com.board.member.service.MemberService;
+import com.board.utils.UriCreator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -32,11 +32,7 @@ public class MemberController {
         Member member = memberMapper.memberPostToMember(requestBody);
 
         Member createdMember = memberService.createMember(member);
-        URI location = UriComponentsBuilder
-                .fromPath(MEMBER_DEFAULT_URL)
-                .path("/{id}")
-                .buildAndExpand(createdMember.getMemberId())
-                .toUri();
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
 
         return ResponseEntity.created(location).build();
     }
