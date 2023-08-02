48cb49d5e91e912e8a37ea845a6ab0b4e935fc88
JoeGukSeon <zipcks1381@gmail.com>
2023-08-03 01:46:28 +0900

    :recycle: refactor : ExceptionCode 수정 INVALID_MEMBER POST_NOT_FOUND 추가

--- src/main/java/com/board/exception/ExceptionCode.java
+++ src/main/java/com/board/exception/ExceptionCode.java
@@ -3,6 +3,8 @@ package com.board.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
+    INVALID_MEMBER(403, "Invalid Member, Cannot Fetch The Post"),
+    POST_NOT_FOUND(404, "Post Not Found"),
     MEMBER_NOT_FOUND(404, "Member Not Found"),
     EMAIL_EXISTS(409, "Email Already Exists");
 
